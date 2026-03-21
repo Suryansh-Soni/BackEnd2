b51c0e4434847eff4ae616f342ce0e4b875ffa36
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-21 15:02:56 +0530

    exports and require

---
+++ Export&require/math.js
@@ -0,0 +1,7 @@
+// module.exports = "Suryansh";
+const pi=3.14
+let obj={
+    sum:3+2,
+    pi:pi
+}
+module.exports=obj
---
+++ Export&require/script.js
@@ -0,0 +1,12 @@
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   console.log(i);
+// }
+
+// args = process.argv;
+// for (i = 2; i < args.length; i++) {
+//   console.log(args[i]);
+// }
+
+let value = require("./math");
+console.log(value);
