9333e6ef2a9054a598c27b5bf2c4d3fe2e584ab0
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-22 15:36:10 +0530

    learned about app.use,get,post,listen,send

---
+++ Express/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+let port = 3000;
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+}); //listen for incomming request on given port
+
+// app.use((req, res) => {
+//   // used to listen all the requests
+
+//   console.log("hello sir .");
+//   //this res can have obj , string anything
+//   //   res.send("this is basic res.");
+//   //   res.send({ name: "suryansh", age: 20 });
+//   //   let code="<h1>html</h1>"
+//   res.send("<h1>html</h1>"); //only one time in a app.use
+// });
+
+// .getfor listening to specific path : any other raoute give not found or any custom error response.
+
+app.get("/", (req, res) => {
+  res.send("you connected root path");
+});
+
+app.get("/apple", (req, res) => {
+  res.send("conected to apple .");
+});
+
+// catch-all route
+app.use((req, res) => {
+  res.status(404).send("This path does not exist");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body); // incoming data
+  res.send("Data received");
+});
