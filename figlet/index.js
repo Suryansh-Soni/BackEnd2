a815118f4006fa7d52033a390a9a4e5f40a4d21d
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-22 14:04:02 +0530

    figlet code

--- figlet/index.js
+++ figlet/index.js
@@ -1,2 +1,33 @@
-const figlet=require("figlet")
-fil
+const figlet = require("figlet");
+
+async function doStuff() {
+  figlet.text("Hello World!!", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+doStuff();
+
+figlet.text(
+  "Boo!",
+  {
+    font: "Ghost",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  },
+  function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }
+);
+
