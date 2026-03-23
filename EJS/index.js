6ce73bdc3e937df59a7910d3581a4b9a30cbba2a
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-23 13:38:01 +0530

    EJS

---
+++ EJS/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path =require("path")
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views",pathjoin(__dirname,"/views"))
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
+
+
