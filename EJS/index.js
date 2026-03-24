1f2d5ad0a0b28ad9a0450a52f73bed274be04e57
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-25 00:01:44 +0530

    using data from other json file (database)

--- EJS/index.js
+++ EJS/index.js
@@ -3,36 +3,29 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-// setup ejs
+const instadata = require("./data.json"); // ✅ move outside
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// routes
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-
-// 🎲 dice route
 app.get("/rolldice", (req, res) => {
   let dice = Math.floor(Math.random() * 6) + 1;
   res.render("rolldice", { num: dice });
 });
 
-// 📸 instagram route
 app.get("/ig/:username", (req, res) => {
-  // const followers = ["adam", "eve", "bob", "steve"];
-  // let { username } = req.params;
-  let {username}=req.params
-  const instadata=require("./data.json")
-  const data=instadata[username]
+  let { username } = req.params;
+  const data = instadata[username];
+  if (!data) {
+    return res.send("User not found ❌");
+  }
   res.render("instagram", { data });
 });
 
-// server start
 app.listen(port, () => {
   console.log(`running on port ${port}`);
 });
