65ab304e9dba06b55783901f352f73021fdb8ba6
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-24 10:56:53 +0530

    interpolation syntax

--- EJS/index.js
+++ EJS/index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const app = express();
-const path =require("path")
+const path = require("path");
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.set("views",pathjoin(__dirname,"/views"))
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
 });
 app.get("/home", (req, res) => {
   res.render("home");
 });
+app.get("/rolldice", (req, res) => {
+  res.render("home");
+});
+
 
 app.listen(port, () => {
   console.log(`running on port ${port}`);
 });
-
-
