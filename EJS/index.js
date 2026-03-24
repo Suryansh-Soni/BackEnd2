e76de8bcbdc05ee3a5fc8d1061e83fad3693b267
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-24 23:40:10 +0530

    printing name of follower list

--- EJS/index.js
+++ EJS/index.js
@@ -3,24 +3,36 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
+// setup ejs
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
 
+// routes
 app.get("/", (req, res) => {
   res.render("home");
 });
+
 app.get("/home", (req, res) => {
   res.render("home");
 });
+
+// 🎲 dice route
 app.get("/rolldice", (req, res) => {
-  let dice = Math.floor(Math.random() * 6);
+  let dice = Math.floor(Math.random() * 6) + 1;
   res.render("rolldice", { num: dice });
 });
+
+// 📸 instagram route
 app.get("/ig/:username", (req, res) => {
-  let { username } = req.params;
-  res.render("instagram.ejs", { username });
+  // const followers = ["adam", "eve", "bob", "steve"];
+  // let { username } = req.params;
+  let {username}=req.params
+  const instadata=require("./data.json")
+  const data=instadata[username]
+  res.render("instagram", { data });
 });
 
+// server start
 app.listen(port, () => {
   console.log(`running on port ${port}`);
 });
