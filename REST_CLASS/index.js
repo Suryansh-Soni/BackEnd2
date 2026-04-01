5214026263a48137f4adf01b1095b1533d0a79b4
Suryansh Soni <suryanshsoni92@gmail.com>
2026-04-01 09:00:16 +0530

    update the content using patch

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-const {v4:uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Set EJS as template engine
 app.set("view engine", "ejs");
@@ -33,15 +34,26 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
   let { username, content } = req.body;
-  let newId= uuidv4();
-  posts.push({id:newId, username, content });
+  let newId = uuidv4();
+  posts.push({ id: newId, username, content });
   res.redirect("/posts");
 });
 
 app.get("/posts/:id", (req, res) => {
-  let {id} = req.params;
+  let { id } = req.params;
   let post = posts.find((p) => p.id === id);
   res.render("show", { post: post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let { content } = req.body;
+  let post = posts.find((p) => p.id === id);
+  if (!post) {
+    return res.status(404).send("Post not found");
+  }
+  post.content = content;
+  res.send("Post updated successfully");
 });
 
 app.listen(port, () => {
