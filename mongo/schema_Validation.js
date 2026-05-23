5ff3238e6d8c8eb459e6b29ad120773aa8614fc9
Suryansh Soni <suryanshsoni92@gmail.com>
2026-05-23 11:06:32 +0530

    connections

--- mongo/schema_Validation.js
+++ mongo/schema_Validation.js
@@ -12,8 +12,13 @@ async function main() {
 
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: Number, required: true ,min:[1,"Price must be at least 1"]},
   description: { type: String, required: true },
+  category: {
+    type: String,
+    enum: ["Fiction", "Non-Fiction", "Science Fiction", "Fantasy"],
+    required: true,
+  },
 });
 
 const Book = mongoose.model("Book", bookSchema);
@@ -21,7 +26,8 @@ const Book = mongoose.model("Book", bookSchema);
 let book1 = new Book({
   title: "The Great Gatsby",
   price: 10.99,
-  description: "A classic novel by F. Scott Fitzgerald."
+  description: "A classic novel by F. Scott Fitzgerald.",
+  category: "Fiction",
 });
 
 book1
@@ -30,3 +36,14 @@ book1
     console.log("Book saved:", res);
   })
   .catch((err) => console.log(err));
+
+Book.findByIdAndUpdate(
+  "6a1002fa8d8178f8a6d60814",
+  { price: 12.99 },
+  { new: true },
+)
+  .then((res) => {
+    console.log("Book updated:", res);
+  })
+  .catch((err) => console.log(err));
+
