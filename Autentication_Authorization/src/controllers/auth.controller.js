6ce383204b8c99008c426199828ef4dda67a3512
Suryansh Soni <suryanshsoni92@gmail.com>
2026-06-29 19:00:47 +0530

    log out all functionallity also log in

--- Autentication_Authorization/src/controllers/auth.controller.js
+++ Autentication_Authorization/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import crypto from "crypto";
+import crypto, { hash } from "crypto";
 import config from "../config/config.js";
 import jwt from "jsonwebtoken";
 import sessionModel from "../models/session.model.js";
@@ -67,6 +67,67 @@ export async function register(req, res) {
       id: newUser._id,
       username: newUser.username,
       email: newUser.email,
+    },
+  });
+}
+
+export async function login(req, res) {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({ message: "No such user" });
+  }
+
+  const hashPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+
+  const isPasswordValid = hashPassword === user.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+
+  const refreshToken = jwt.sign({ userId: user._id }, config.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex");
+
+  const session = await sessionModel.create({
+    user: user._id,
+    refreshTokenHash,
+    ip: req.ip || "Unknown",
+    userAgent: req.get("user-agent") || "Unknown",
+  });
+
+  const accessToken = jwt.sign(
+    { userId: user._id, sessionId: session._id },
+    config.JWT_SECRET,
+    { expiresIn: "15m" },
+  );
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    message: "Login successful",
+    accessToken,
+    sessionId: session._id,
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
     },
   });
 }
@@ -167,4 +228,27 @@ export async function logout(req, res) {
   res.clearCookie("refreshToken");
 
   res.status(200).json({ message: "Logout successful" });
+}
+
+export async function logoutAll(req, res) {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return res.status(400).json({
+      message: "Refresh token not found",
+    });
+  }
+  const decoded = jwt.verify(refreshToken, config.JWT_SECRET);
+  await sessionModel.updateMany(
+    {
+      user: decoded.id,
+      revoked: false,
+    },
+    {
+      revoked: true,
+    },
+  );
+  res.clearCookie("refreshToken");
+  res.status(200).json({
+    message: "Logged out successfully from all devices",
+  });
 }
