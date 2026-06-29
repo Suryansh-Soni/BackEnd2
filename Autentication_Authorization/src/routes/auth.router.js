bbbd2e3f76c306b3b1014a36e105aa50d7d9d50d
Suryansh Soni <suryanshsoni92@gmail.com>
2026-06-29 21:57:43 +0530

    created new route -> verify-email

--- Autentication_Authorization/src/routes/auth.router.js
+++ Autentication_Authorization/src/routes/auth.router.js
@@ -7,6 +7,7 @@ authRouter.post("/login",authController.login);
 authRouter.get("/get-me", authController.getMe);
 authRouter.get("/refresh-token", authController.refreshToken);
 authRouter.get("/logout",authController.logout);
-authRouter.get("/logout-all",authController.logoutAll)
+authRouter.get("/logout-all",authController.logoutAll);
+authRouter.get("/verify-email",authController.verifyEmail);
 
 export default authRouter; 
