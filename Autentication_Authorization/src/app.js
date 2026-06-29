f9d162b2fda2384eff0e4509560974e709f398c2
Suryansh Soni <suryanshsoni92@gmail.com>
2026-06-29 15:34:03 +0530

    refresh,accesstoken- app.js

--- Autentication_Authorization/src/app.js
+++ Autentication_Authorization/src/app.js
@@ -3,14 +3,15 @@ import mongoose from "mongoose";
 //morgan is a logger middleware for node.js HTTP requests
 import morgan from "morgan";
 import authRouter from "./routes/auth.router.js";
+import cookieParser from "cookie-parser";
 
-const app=express();
+const app = express();
 
-app.use(morgan("dev"));   
 app.use(express.json());
-app.use("/api/auth",authRouter);    
-
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/auth", authRouter);
 
 export default app;
